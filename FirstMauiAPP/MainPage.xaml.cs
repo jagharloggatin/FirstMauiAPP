d4aea29c5d3fb066b8679a75d8e00a87e2a0be78
Jonathan <89255112+jagharloggatin@users.noreply.github.com>
2023-01-19 12:28:43 +0100

    Add project files.

---
+++ FirstMauiAPP/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using FirstMauiAPP.ViewModel;
+
+namespace FirstMauiAPP;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage(MainViewModel vm)
+	{
+		InitializeComponent();
+		BindingContext = vm;
+
+	}
+    //int count = 0;
+
+    //private void OnCounterClicked(object sender, EventArgs e)
+    //{
+    //	count++;
+
+    //	if (count == 1)
+    //		CounterBtn.Text = $"Clicked {count} time";
+    //	else
+    //		CounterBtn.Text = $"Clicked {count} times";
+
+    //	SemanticScreenReader.Announce(CounterBtn.Text);
+    //}
+}
+
---
+++ FirstMauiAPP/ViewModel/MainViewModel.cs
@@ -0,0 +1,74 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MvvmHelpers.Commands;
+using PropertyChanged;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+
+namespace FirstMauiAPP.ViewModel
+{
+    [AddINotifyPropertyChangedInterface]
+    public partial class MainViewModel : ObservableObject
+    {
+        public bool IsRefresh { get; set; }
+        //public AsyncCommand RefreshCommand { get; }
+        //public AsyncCommand ClearAllCommand { get; }
+
+        [ObservableProperty]
+        ObservableCollection<string> items;
+
+        [ObservableProperty]
+        string text;
+
+        [ObservableProperty]
+        bool isRefreshing;
+
+        public MainViewModel()
+        {
+            //RefreshCommand = new AsyncCommand(Refresh);
+            //ClearAllCommand = new RelayCommand(ClearAll);
+            Items = new ObservableCollection<string>();
+        }
+
+        public async Task<ObservableCollection<string>> GetTodoItems()
+        {
+            if (Items?.Count > 0)
+                return Items;
+
+            return Items;
+        }
+
+        [RelayCommand]
+        void Add()
+        {
+            if (string.IsNullOrWhiteSpace(Text))
+                return;
+
+            Items.Add(text);
+            //add our item
+            Text = string.Empty;
+        }
+
+        [RelayCommand]
+        void Remove(string s)
+        {
+            if (Items.Contains(s))
+                Items.Remove(s);
+        }
+
+        [RelayCommand]
+        void ClearAllItems()
+        {
+            if (Items.Count > 0)
+                Items.Clear();
+        }
+
+        [RelayCommand]
+        async Task Refresh()
+        {
+            isRefreshing = true;
+            await Task.Delay(1000);
+            isRefreshing = false;
+        }
+    }
+}
